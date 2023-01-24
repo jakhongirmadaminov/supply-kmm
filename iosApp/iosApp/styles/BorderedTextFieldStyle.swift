255a0b401e86ffd46a6264d00517aec989ddef9e
Jakhongir Madaminov <Jakhongir.Madaminov@cantire.com>
2023-01-24 17:50:19 +0500

    under construction

--- iosApp/iosApp/styles/BorderedTextFieldStyle.swift
+++ iosApp/iosApp/styles/BorderedTextFieldStyle.swift
@@ -5,5 +5,16 @@
 //  Created by Jakhongir Madaminov on 24/01/23.
 //  Copyright © 2023 orgName. All rights reserved.
 //
-
 import Foundation
+import SwiftUI
+
+struct BorderedTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(15)
+            .background(
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .stroke(Color.gray, lineWidth: 0.5)
+            )
+    }
+}
