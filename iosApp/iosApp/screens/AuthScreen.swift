132c4e4224b86a0c051273769abf808eb3ba56bc
Jakhongir Madaminov <Jakhongir.Madaminov@cantire.com>
2023-01-24 16:49:18 +0500

    under construction

--- iosApp/iosApp/screens/AuthScreen.swift
+++ iosApp/iosApp/screens/AuthScreen.swift
@@ -11,11 +11,47 @@ import shared
 
 struct AuthScreen : View{
     
+    @State var textFieldText :String = ""
+    
     var body : some View{
-        VStack{
+        VStack(
+            alignment: .leading
+        ) {
+            Spacer().frame(height: 25)
             Text(MR.strings().authorization.desc().localized())
                 .foregroundColor(Color("PrimaryTextColor"))
                 .font(Font.custom("Mulish", size:30).weight(.heavy))
-        }
+                .frame( maxWidth: .infinity,alignment: .topLeading)
+                .padding(.horizontal, 25.0)
+            
+            Spacer().frame(height: 25)
+            
+            Text(MR.strings().authorization_info.desc().localized())
+                    .foregroundColor(Color("PrimaryTextColor"))
+                    .font(Font.custom("Mulish", size:14).weight(.medium))
+                    .frame( maxWidth: .infinity, alignment: .topLeading)
+                    .padding(.horizontal, 25.0)
+            
+            Spacer().frame(height: 20)
+            Text(MR.strings().phone_number.desc().localized())
+                    .foregroundColor(Color("PrimaryTextColor"))
+                    .font(Font.custom("Mulish", size:14).weight(.medium))
+                    .frame( maxWidth: .infinity, alignment: .topLeading)
+                    .padding(.horizontal, 25.0)
+            
+            TextField("+998", text: $textFieldText)
+                .padding()
+                .cornerRadius(10)
+            
+            
+            
+        }.frame(maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+            .background(Color.blue)
+    }
+}
+
+struct AuthScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthScreen()
     }
 }
