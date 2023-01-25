585c1446de92551ef320ce82201fb9dacd6448d7
mbaxriddinov <mbaxriddinov@cdti.uz>
2023-01-25 18:54:33 +0500

    removed some files

--- shared/src/commonMain/kotlin/uz/uzkass/smartpos/supply/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package uz.uzkass.smartpos.supply
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greeting(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
