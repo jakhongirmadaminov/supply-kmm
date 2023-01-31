32cac90178b99b37c267aed7becdb43649012f74
mbaxriddinov <mbaxriddinov@cdti.uz>
2023-01-31 19:17:18 +0500

    removed demo files

--- shared/src/commonMain/kotlin/uz/uzkass/smartpos/supply/services/demo/DemoService.kt
+++
@@ -1,9 +0,0 @@
-package uz.uzkass.smartpos.supply.services.demo
-
-import uz.uzkass.smartpos.supply.core.data.LoginRequest
-
-interface DemoService {
-
-    suspend fun login(loginRequest: LoginRequest): String
-
-}
