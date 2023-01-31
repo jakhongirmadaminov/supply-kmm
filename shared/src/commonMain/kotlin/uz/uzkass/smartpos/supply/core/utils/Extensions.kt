861600a4cea2e23f62f798620c284670e560487b
jahongir9779 <jahongir9779@gmail.com>
2023-01-31 17:42:00 +0500

    some changes

---
+++ shared/src/commonMain/kotlin/uz/uzkass/smartpos/supply/core/utils/Extensions.kt
@@ -0,0 +1,14 @@
+package uz.uzkass.smartpos.supply.core.utils
+
+import kotlinx.coroutines.CancellationException
+
+
+internal inline fun <T, R> T.resultOf(block: T.() -> R): Result<R> {
+    return try {
+        Result.success(block())
+    } catch (e: CancellationException) {
+        throw e
+    } catch (e: Exception) {
+        Result.failure(e)
+    }
+}
