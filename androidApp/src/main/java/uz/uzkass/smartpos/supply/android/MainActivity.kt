f0cc01a5c686e4f90fb86b5ed8f980d34f382155
mbaxriddinov <mbaxriddinov@cdti.uz>
2023-02-02 12:14:48 +0500

    minor fix

--- androidApp/src/main/java/uz/uzkass/smartpos/supply/android/MainActivity.kt
+++ androidApp/src/main/java/uz/uzkass/smartpos/supply/android/MainActivity.kt
@@ -3,9 +3,6 @@ package uz.uzkass.smartpos.supply.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import dev.icerock.moko.errors.mappers.ExceptionMappersStorage
-import dev.icerock.moko.network.errors.registerAllNetworkMappers
-import uz.uzkass.smartpos.supply.android.coreui.TimerTextButton
 import uz.uzkass.smartpos.supply.android.ui.SupplyAppCompose
 import uz.uzkass.smartpos.supply.android.ui.theme.SupplyTheme
 
@@ -14,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            SupplyTheme() {
+            SupplyTheme {
                 SupplyAppCompose()
             }
         }
